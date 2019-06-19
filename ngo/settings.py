094082c20b249010913b40e73487cee21cb17c07
venothx7 <venothx7@hotmail.com>
2019-06-19 12:21:44 -0400

    routing and styling

--- ngo/settings.py
+++ ngo/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), '/static/',]
 LOGIN_REDIRECT_URL = '/login_success'
 LOGOUT_REDIRECT_URL = '../login'
--- ngo/urls.py
+++ ngo/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings 
+from django.conf.urls.static import static 
 from django.contrib.auth import views as auth_views
 from user.views import SignUp, login_success
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,8 +13,9 @@ urlpatterns = [
     path(r'event/', include('event.urls')),
     # path(r'event/userview', include('event.urls')),
     path('register/', include('register.urls')),
-    path(r'', 
-        auth_views.LoginView.as_view(template_name='registration/login.html'),
+    path(r'',auth_views.LoginView.as_view(template_name='registration/login.html'),
+        name ='login_root'),
+    path(r'login/',auth_views.LoginView.as_view(template_name='registration/login.html'),
         name ='login'),
     path(r'logout/', 
         auth_views.LogoutView.as_view(template_name='registration/login.html'), 
@@ -19,9 +23,13 @@ urlpatterns = [
     path('signup/', SignUp.as_view(), name ='signup' ),
     path('login_success/', login_success, name ='login_success' ),
 
-
+    
     # path('register/', include('register.urls')),
 
 
 
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                                document_root = settings.MEDIA_ROOT)
